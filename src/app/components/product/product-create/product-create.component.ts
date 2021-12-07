5fa4e27a31ec81e5a1a383174e83d5d158b2dadc
rrricardo00 <rrricardo00@yahoo.com.br>
2021-12-06 23:43:55 -0300

    angular

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../product.service';
@@ -9,6 +10,11 @@ import { ProductService } from '../product.service';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: 'Produto de teste',
+    price: 125.23
+  }
+
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
@@ -16,7 +22,9 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage("Operação executada com sucesso")
+
+    this.productService.create(this.product).subscribe(() => this.productService.showMessage("Operação executada com sucesso"))
+    this.cancel()
   }
 
   cancel(): void {
