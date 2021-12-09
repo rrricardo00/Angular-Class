878a3836348b65d8fd90ca0afc977b9e423d8e4a
rrricardo00 <rrricardo00@yahoo.com.br>
2021-12-09 00:27:57 -0300

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule } from "@angular/material/input"
+import { MatInputModule } from "@angular/material/input";
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { MatInputModule } from "@angular/material/input"
     ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
